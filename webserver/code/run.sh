d341e8f3050de1d44b1a2ed388e6e86eea587587
Blinov Ilya <ulkhan.2014@mail.ru>
2023-02-24 14:35:50 +0300

    Change port to 8002

--- webserver/code/run.sh
+++ webserver/code/run.sh
@@ -11,6 +11,6 @@
 
 export APP_MODULE=${APP_MODULE-app.main:app}
 export HOST=${HOST:-0.0.0.0}
-export PORT=${PORT:-8001}
+export PORT=${PORT:-8002}
 
 exec gunicorn --bind $HOST:$PORT "$APP_MODULE" -k uvicorn.workers.UvicornWorker
