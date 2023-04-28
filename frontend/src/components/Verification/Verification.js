019b015d34e7e50f87dc9edfacc99ee59c76e606
anzhelika <71946038+fuwa-fuwaru@users.noreply.github.com>
2023-04-28 23:28:52 +0300

    edit form and verification scripts

--- frontend/src/components/Verification/Verification.js
+++ frontend/src/components/Verification/Verification.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react"
 import "./Verification.css"
 import { Button } from "react-bootstrap"
 import axios from "axios";
+//import CookieLib from "../../cookielib-main/index.js"
 import CookieLib from "../../cookielib/index.js"
 
 let result = 'await'  // 'await' is the default state, then becomes 'mode_success' or 'mode_expired'
@@ -20,43 +21,51 @@ const getStatus = async() => {
     let request_url = window.location.pathname;
     let request_id_encrypted = request_url.substring(request_url.lastIndexOf('/') + 1);
     //console.log(request_url);
-    //console.log(CookieLib.getCookieToken());
+    console.log('prev token: ', CookieLib.getCookieToken());
     if (request_url.includes('signup')) {
-        let url = 'http://127.0.0.1/registration/verify/' + request_id_encrypted;
+        //let url = 'http://127.0.0.1/registration/verify/' + request_id_encrypted;
+        let url = '/registration/verify/' + request_id_encrypted;
         await axios.post(url, {
             request_id_encrypted: request_id_encrypted,
-        })
-            .then((response) => {
-                result = 'signup_success'
-                console.log(response);
-                //console.log(response.status);
-                let token = response.data('token') // ?????????
-                CookieLib.setCookieToken(token)
-                //console.log(CookieLib.getCookieToken());
-            })
-            .catch((error) => {
-                result = 'signup_expired'
-                console.log(error.response.status);
-                //console.log(CookieLib.getCookieToken());
-            });
+        }).then(response => {
+            result = 'signup_success'
+            console.log('post response: ', response);
+            console.log('result in post then: ', result);
+            //console.log(response.status);
+            //console.log('token in response: ', response.data('token'));
+            console.log('token in response: ', response.data.token);
+            //let token = response.data('token') // response.data.token???
+            let token = response.data.token;
+            CookieLib.setCookieToken(token);
+            console.log('new token then: ', CookieLib.getCookieToken());
+        }).catch(error => {
+            console.log(error);
+            result = 'signup_expired'
+            console.log(error.response/*.status*/);
+            console.log('result in post catch: ', result);
+            console.log('new token catch: ', CookieLib.getCookieToken());
+        });
     } else {
-        let url = 'http://127.0.0.1/login/verify/' + request_id_encrypted;
+        //let url = 'http://127.0.0.1/login/verify/' + request_id_encrypted;
+        let url = '/login/verify/' + request_id_encrypted;
         await axios.post(url, {
             request_id_encrypted: request_id_encrypted,
+        }).then((response) => {
+            result = 'login_success'
+            console.log(response);
+            console.log('result in post then: ', result);
+            //console.log(response.status);
+            //let token = response.data('token') // ?????????
+            let token = response.data.token;
+            CookieLib.setCookieToken(token)
+            //console.log(CookieLib.getCookieToken());
         })
-            .then((response) => {
-                result = 'login_success'
-                console.log(response);
-                //console.log(response.status);
-                let token = response.data('token') // ?????????
-                CookieLib.setCookieToken(token)
-                //console.log(CookieLib.getCookieToken());
-            })
-            .catch((error) => {
-                result = 'login_expired'
-                console.log(error.response.status);
-                //console.log(CookieLib.getCookieToken());
-            });
+        .catch((error) => {
+            result = 'login_expired'
+            console.log(error.response/*.status*/);
+            console.log('result in post catch: ', result);
+            //console.log(CookieLib.getCookieToken());
+        });
     }
 }
 
@@ -66,9 +75,11 @@ const Verification = ({handleClick}) => {   // or login???
         getStatus().then(
             res => setResult(result));
             //result => setResult(res));
-            //console.log(res);
-            console.log(result);
-    }/*,[res]*/);
+            console.log('res: ', res);
+            console.log('result: ', result);
+    //}/*,[res]*/);
+    },[]); // does 1 request
+    //},[res]);  // does 2 requests
     if (result === 'signup_success') {
         return (
             <div className="content">
