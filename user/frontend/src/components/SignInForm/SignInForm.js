0f5f930259bb9d72107e3ea469cc58babfe9288a
anzhelika <71946038+fuwa-fuwaru@users.noreply.github.com>
2023-04-30 20:41:28 +0300

    clean up verification and signinform

--- user/frontend/src/components/SignInForm/SignInForm.js
+++ user/frontend/src/components/SignInForm/SignInForm.js
@@ -3,15 +3,14 @@ import "./SignInForm.css"
 import { Button } from "react-bootstrap"
 import "bootstrap/dist/js/bootstrap.js"
 import axios from "axios"
-//import CookieLib from "../../cookielib-main/index.js"
 import CookieLib from "../../cookielib/index.js"
 
 let mode = 'signup'
 let contact_type = 'email'
-let status = 'await' // await_login/await_signup/success/signup_taken/logged_in/login_nonexistent/error
+let status = 'await'
 
 
-function switchToEmail() { // ADD A CONTACT_FLAG
+function switchToEmail() {
     let item = document.getElementById('tg');
     item.style.display='none';
     item = document.getElementById('email');
@@ -29,100 +28,15 @@ function switchToTelegram() {
 
 function switchMode() { // ADD A MODE_FLAG
     if (mode === 'signup') {  // check by mode
-        /*let content = document.getElementById('button_switch_mode');
-        content.innerHTML = 'Регистрация';
-        content = document.getElementById('text_mode');
-        content.innerHTML = 'Авторизация';
-        content = document.getElementById('button_submit');
-        content.innerHTML = 'Авторизоваться';*/
         mode = 'login'
         window.location.pathname = '/login'
     } else {
-        /*let content = document.getElementById('button_switch_mode');
-        content.innerHTML = 'Авторизация';
-        content = document.getElementById('text_mode');
-        content.innerHTML = 'Регистрация';
-        content = document.getElementById('button_submit');
-        content.innerHTML = 'Зарегистрироваться';*/
         mode = 'signup'
         window.location.pathname = '/signup'
     }
 }
 
-async function handleSubmitClick() { // ADD A MODE_FLAG
-    let content = document.getElementById('text_mode');
-    content.innerHTML = 'На вашу почту отправлено письмо подтверждения.'; // а если не почта а тг???
-    content = document.getElementById('email');
-    content.style.display='none';
-    content = document.getElementById('tg');
-    content.style.display='none';
-    content = document.getElementById('button_switch_mode');
-    content.style.display='none';
-    content = document.getElementById('button_return');
-    //let input;
-    let data;
-    if (contact_type === 'email') {
-        content.innerHTML = 'Ввести почту заново'
-        // get input data for mail
-        /*input = document.getElementById('email_input');
-        data = input.value;*/
-        data = document.getElementById('email_input').value;
-    } else {
-        // get input data for tg
-        content.innerHTML = 'Ввести юзернейм заново'
-        /*input = document.getElementById('tg_input');
-        data = input.value;*/
-        data = document.getElementById('tg_input').value;
-    }
-    content.style.display='flex';
-    content = document.getElementById('button_submit');
-    content.innerHTML = 'Отправить письмо еще раз'
-    // send email to given address and receive new token for cookies
-    if (mode === 'signup') { // WORKS, USE AS REF FOR LOGIN
-        let token = CookieLib.getCookieToken();
-        console.log('token: ', token);
-        if (token === undefined) {  // if undefined, post request to user to trigger anonymous registration
-            //await axios.post('http://127.0.0.1/new-token', {})
-            //await axios.post('http://127.0.0.1/new_token', {})
-            await axios.post('/new_token', {
-            }).then(response => {
-                console.log('new_token response: ', response);
-                //console.log(response.status);
-                token = response.data
-                CookieLib.setCookieToken(token)
-                console.log('token after new_token: ', CookieLib.getCookieToken());
-            }).catch(error => {
-                console.log(error.response/*.status*/);
-                //console.log(CookieLib.getCookieToken());
-            });
-        }
-        //console.log(CookieLib.getCookieToken());
-        //CookieLib.removeCookie();
-        //axios.post("http://127.0.0.1/registration", {
-        await axios.post("/registration", {
-            email_address: data,
-        }, {
-            params: {
-                //user_token: ',vnbsdkjbfojvs;mb;fl',
-                user_token: token,
-            },
-        })
-            .then((response) => {
-                console.log('registration response: ', response);
-            });
-    } else {
-        //axios.post("http://127.0.0.1/login", { // no params for login?
-        //await axios.post("/login", {
-        await axios.post("/authorization", {
-                email_address: data,
-            })
-            .then((response) => {
-                console.log('login response: ', response);
-            });
-    }
-}
-
-async function afterSubmit() {
+async function handleSubmitClick() {
     let data;
     if (contact_type === 'email') {
         data = document.getElementById('email_input').value;
@@ -132,114 +46,73 @@ async function afterSubmit() {
     //requests
     if (mode === 'signup') {
         let token = CookieLib.getCookieToken();
-        console.log('token: ', token);
-        if (token === undefined) {  // if undefined, post request to user to trigger anonymous registration
-            //await axios.post('http://127.0.0.1/new-token', {})
-            //await axios.post('http://127.0.0.1/new_token', {})
+        if (token === undefined) {
             await axios.post('/new_token', {
             }).then(response => {
-                console.log('new_token response: ', response);
-                //console.log(response.status);
-                token = response.data // ?????????
+                token = response.data
                 CookieLib.setCookieToken(token)
-                console.log('token after new_token: ', CookieLib.getCookieToken());
             }).catch(error => {
-                console.log(error.response/*.status*/);
-                //console.log(CookieLib.getCookieToken());
-                // GENERAL_ERROR - Проблемы на сервере, пожалуйста, попробуйте позже
                 status = 'error';
-                console.log('status: ', status)
             });
         }
-        //console.log(CookieLib.getCookieToken());
-        //CookieLib.removeCookie();
-        //axios.post("http://127.0.0.1/registration", {
         await axios.post("/registration", {
             email_address: data,
         }, {
             params: {
-                //user_token: ',vnbsdkjbfojvs;mb;fl',
                 user_token: token,
             },
         }).then(response => {
-            console.log('registration response: ', response);
-            //if (response.data.detail === 'You are already logged in with this email') {
-            if (response.data === null) {  // response.data.detail raises error when data = null
+            if (response.data === null) {
                 status = 'success';
-                console.log('status: ', status);
             } else {
-                //if (response.data.detail === 'You are already logged in with this email') status = logged_in
                 status = 'logged_in';
-                console.log('status: ', status)
             }
-        }).catch(error => {   // triggers when email is not valid (error 422!!!)
-            console.log(error);
-            console.log(error.response/*.status*/);
-            //console.log(CookieLib.getCookieToken());
-            // error 409 - signup_taken
-            console.log(error.response.status);
+        }).catch(error => {
             if (error.response.status === 422) {
-                status = 'incorrect_format'; /// некорректный формат !!!!!!!
+                status = 'incorrect_format';
             } else if (error.response.status === 409) {
                 status = 'signup_taken';
             } else {
                 status = 'error';
             }
-            //status = 'signup_taken';
-            console.log('status: ', status)
         });
     } else {
-        //axios.post("http://127.0.0.1/login", { // no params for login?
-        //await axios.post("/login", {
         await axios.post("/authorization", {
             email_address: data,
         }).then(response => {
-            console.log('login response: ', response);
-            if (response.data === null) {  // response.data.detail raises error when data = null
+            if (response.data === null) {
                 status = 'success';
-                console.log('status: ', status);
             } else {
-                //if (response.data.detail === 'You are already logged in with this email') status = logged_in
                 status = 'logged_in';
-                console.log('status: ', status)
             }
         }).catch(error => {
-            console.log(error);
-            console.log(error.response/*.status*/);
-            //console.log(CookieLib.getCookieToken());
-            // error 409 - signup_taken ???
-            console.log(error.response.status);
             if (error.response.status === 422) {
-                status = 'incorrect_format'; /// некорректный формат !!!!
+                status = 'incorrect_format';
             } else if (error.response.status === 404) {
                 status = 'login_nonexistent';
             } else {
                 status = 'error';
             }
-            //status = 'login_nonexistent';
-            console.log('status: ', status)
         });
     }
-    let content = document.getElementById('email'); // form for email, removed anyway
+    let content = document.getElementById('email');
     content.style.display='none';
 
-    content = document.getElementById('tg'); // form for tg, removed anyway
+    content = document.getElementById('tg');
     content.style.display='none';
 
     if (status === 'logged_in') {
         content = document.getElementById('text_mode');
         content.innerHTML = 'Вы уже авторизованы.<br/>Тесты доступны на верхней панели.<br/>Чтобы принять участие в исследовании, пожалуйста, заполните анкету.';
-        // кнопка для анкеты?
 
         content = document.getElementById('button_submit');
         content.style.display='none';
         content = document.getElementById('button_switch_mode');
         content.style.display='none';
 
-    } else if (status === 'signup_taken') {  // triggers when email is not valid (error 422!!!)
+    } else if (status === 'signup_taken') {
         content = document.getElementById('text_mode');
-        content.innerHTML = 'Введенная вами почта уже занята.'; // а если не почта а тг???
-        // 2 кнопки - ввести другую почту(рега!!!) И Авторизоваться
+        content.innerHTML = 'Введенная вами почта уже занята.';
 
         content = document.getElementById('button_submit');
         content.style.display='none';
@@ -247,26 +120,20 @@ async function afterSubmit() {
         content = document.getElementById('button_return');
         if (contact_type === 'email') {
             content.innerHTML = 'Ввести почту заново'
-            //data = document.getElementById('email_input').value;
         } else {
-            // get input data for tg
             content.innerHTML = 'Ввести юзернейм заново'
-            //data = document.getElementById('tg_input').value;
         }
         content.style.display='flex';
 
     } else if (status === 'login_nonexistent') {
         content = document.getElementById('text_mode');
-        content.innerHTML = 'Данная почта не зарегистрирована.'; // а если не почта а тг???
+        content.innerHTML = 'Данная почта не зарегистрирована.';
 
         content = document.getElementById('button_return');
         if (contact_type === 'email') {
             content.innerHTML = 'Ввести почту заново'
-            //data = document.getElementById('email_input').value;
         } else {
-            // get input data for tg
             content.innerHTML = 'Ввести юзернейм заново'
-            //data = document.getElementById('tg_input').value;
         }
         content.style.display='flex';
 
@@ -279,14 +146,11 @@ async function afterSubmit() {
         content = document.getElementById('button_switch_mode');
         content.style.display='none';
         content = document.getElementById('button_return');
-        //let data;  // DATA
+
         if (contact_type === 'email') {
             content.innerHTML = 'Ввести почту заново'
-            //data = document.getElementById('email_input').value;
         } else {
-            // get input data for tg
             content.innerHTML = 'Ввести юзернейм заново'
-            //data = document.getElementById('tg_input').value;
         }
         content.style.display='flex';
         content = document.getElementById('button_submit');
@@ -298,17 +162,14 @@ async function afterSubmit() {
         content = document.getElementById('button_return');
         if (contact_type === 'email') {
             content.innerHTML = 'Ввести почту заново'
-            //data = document.getElementById('email_input').value;
         } else {
-            // get input data for tg
             content.innerHTML = 'Ввести юзернейм заново'
-            //data = document.getElementById('tg_input').value;
         }
         content.style.display='flex';
 
         content = document.getElementById('button_submit');
         content.style.display='none';
-    } else {  // if error
+    } else {
         content = document.getElementById('text_mode');
         content.innerHTML = 'Произошла ошибка. Пожалуйста, попробуйте позднее.';
         content = document.getElementById('button_switch_mode');
@@ -318,7 +179,7 @@ async function afterSubmit() {
     }
 }
 
-function handleReturnClick() { // BUTTON_RETURN
+function handleReturnClick() {
     let content = document.getElementById('button_return');
     content.style.display='none';
     if (mode === 'signup') {
@@ -339,7 +200,6 @@ function handleReturnClick() { // BUTTON_RETURN
         content.innerHTML = 'Регистрация'
     }
     //content.style.display='flex';
-
     if (contact_type === 'email') {
         content = document.getElementById('email');
         content.style.display='flex';
@@ -349,9 +209,6 @@ function handleReturnClick() { // BUTTON_RETURN
     }
     content = document.getElementById('button_switch_mode');
     content.style.display='flex';
-    /*content = document.getElementById('button_return');
-    content.style.display='none';*/
-
 }
 
 const SignInForm = ({ handleClick }) => {
@@ -389,7 +246,7 @@ const SignInForm = ({ handleClick }) => {
                         <input type="text" className="form-control" placeholder="Username" id="tg_input" aria-label="Text input with dropdown button"/>
                     </div>
                     <div className="submit">
-                        <Button variant="success" id="button_submit" onClick={afterSubmit}>
+                        <Button variant="success" id="button_submit" onClick={handleSubmitClick}>
                             Зарегистрироваться
                         </Button>
                     </div>
@@ -432,7 +289,7 @@ const SignInForm = ({ handleClick }) => {
                         <input type="text" className="form-control" placeholder="Username" id="tg_input" aria-label="Text input with dropdown button"/>
                     </div>
                     <div className="submit">
-                        <Button variant="success" id="button_submit" onClick={afterSubmit}>
+                        <Button variant="success" id="button_submit" onClick={handleSubmitClick}>
                             Авторизоваться
                         </Button>
                     </div>
--- user/frontend/src/components/Verification/Verification.js
+++ user/frontend/src/components/Verification/Verification.js
@@ -2,13 +2,9 @@ import React, {useState, useEffect} from "react"
 import "./Verification.css"
 import { Button } from "react-bootstrap"
 import axios from "axios";
-//import CookieLib from "../../cookielib-main/index.js"
 import CookieLib from "../../cookielib/index.js"
 
-let result = 'await'  // 'await' is the default state, then becomes 'mode_success' or 'mode_expired'
-//let mode = 'signup'
-//let contact_type = 'email' // or tg for the future
-
+let result = 'await'
 function redirectSignUp() {
     window.location.pathname = "/signup"
 }
@@ -20,67 +16,38 @@ function redirectLogin() {
 const getStatus = async() => {
     let request_url = window.location.pathname;
     let request_id_encrypted = request_url.substring(request_url.lastIndexOf('/') + 1);
-    //console.log(request_url);
-    console.log('prev token: ', CookieLib.getCookieToken());
     if (request_url.includes('signup')) {
-        //let url = 'http://127.0.0.1/registration/verify/' + request_id_encrypted;
         let url = '/registration/verify/' + request_id_encrypted;
         await axios.post(url, {
             request_id_encrypted: request_id_encrypted,
         }).then(response => {
             result = 'signup_success'
-            console.log('post response: ', response);
-            console.log('result in post then: ', result);
-            //console.log(response.status);
-            //console.log('token in response: ', response.data('token'));
-            console.log('token in response: ', response.data.token);
-            //let token = response.data('token') // response.data.token???
             let token = response.data.token;
             CookieLib.setCookieToken(token);
-            console.log('new token then: ', CookieLib.getCookieToken());
         }).catch(error => {
-            console.log(error);
             result = 'signup_expired'
-            console.log(error.response/*.status*/);
-            console.log('result in post catch: ', result);
-            console.log('new token catch: ', CookieLib.getCookieToken());
         });
     } else {
-        //let url = 'http://127.0.0.1/login/verify/' + request_id_encrypted;
-        //let url = '/login/verify/' + request_id_encrypted;
         let url = '/authorization/verify/' + request_id_encrypted;
         await axios.post(url, {
             request_id_encrypted: request_id_encrypted,
-        }).then((response) => {
+        }).then(response => {
             result = 'login_success'
-            console.log(response);
-            console.log('result in post then: ', result);
-            //console.log(response.status);
-            //let token = response.data('token') // ?????????
             let token = response.data.token;
             CookieLib.setCookieToken(token)
-            //console.log(CookieLib.getCookieToken());
         })
-        .catch((error) => {
+        .catch(error => {
             result = 'login_expired'
-            console.log(error.response/*.status*/);
-            console.log('result in post catch: ', result);
-            //console.log(CookieLib.getCookieToken());
         });
     }
 }
 
-const Verification = ({handleClick}) => {   // or login???
+const Verification = ({handleClick}) => {
     const [res, setResult] = useState("await");
     useEffect(() => {
         getStatus().then(
             res => setResult(result));
-            //result => setResult(res));
-            console.log('res: ', res);
-            console.log('result: ', result);
-    //}/*,[res]*/);
-    },[]); // does 1 request
-    //},[res]);  // does 2 requests
+    },[]);
     if (result === 'signup_success') {
         return (
             <div className="content">
@@ -110,7 +77,7 @@ const Verification = ({handleClick}) => {   // or login???
                 </div>
             </div>
         )
-    } else if (result === 'login_success') { // ПРОВЕРКА ЗАПОЛНЕННОСТИ АНКЕТЫ
+    } else if (result === 'login_success') {
         return (
             <div className="content">
                 <div className="frame" style={{height: '340px'}}>
@@ -118,9 +85,7 @@ const Verification = ({handleClick}) => {   // or login???
                         Вы авторизованы. Рады снова видеть вас на Burnout tester!<br/>Тесты доступны на верхней
                         панели.<br/>Чтобы принять участие в исследовании, пожалуйста, заполните анкету.
                     </div>
-                    <Button variant="success"
-                            onClick={() => window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
-                            id="button_quiz">
+                    <Button variant="success" onClick={redirectSignUp} id="button_quiz">
                         Заполнить анкету
                     </Button>
                 </div>
@@ -134,7 +99,7 @@ const Verification = ({handleClick}) => {   // or login???
                         Срок действия ссылки истек.<br/> Пожалуйста, авторизуйтесь заново.
                     </div>
                     <Button variant="success" onClick={redirectLogin} id="button_repeat">
-                        Повторить регистрацию
+                        Повторить авторизацию
                     </Button>
                 </div>
             </div>
